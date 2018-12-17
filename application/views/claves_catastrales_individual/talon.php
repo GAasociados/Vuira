34032cf788070a8409718e0ddc5cff8127eca5f0
Dante <dante02d@gmail.com>
2018-12-16 19:26:22 -0600

    Correccion de Meses en el talon al idioma espanol

--- application/views/claves_catastrales_individual/talon.php
+++ application/views/claves_catastrales_individual/talon.php
@@ -1,3 +1,10 @@
+
+<?php
+setlocale(LC_ALL,"es_ES");
+$meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+$fechaEntrega=date('d',strtotime("" . $fechafinal))." de ".$meses[date('m',strtotime("" . $fechafinal))-1]. " del ".date('Y',strtotime("" . $fechafinal))." ".date("H:i A",strtotime("" . $fechafinal));
+
+ ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,16 +15,16 @@
             }
             th, td {
                 padding: 5px;
-                font-size: 20px;   
+                font-size: 20px;
             }
 
-            .cuadro{ 
-                width:1400px; 
-                height:200px; 
-                background:#F4F6F7; 
-                font-family:arial; 
-                font-weight:bold; 
-                color:#212121; 
+            .cuadro{
+                width:1400px;
+                height:200px;
+                background:#F4F6F7;
+                font-family:arial;
+                font-weight:bold;
+                color:#212121;
                 border-radius: 25px;
             }
             label{
@@ -32,39 +39,33 @@
         <div class="cuadro">
             <table style="width:100%">
                 <tr>
-                    <th colspan="3" align="center">Clave Catastral</th>
+                    <td align="center">Clave Catastral</th>
                 </tr>
                 <tr>
-                    <td rowspan="5" width="180" align=" center"><img src="<?php echo base_url('assets/images/logo.png   ') ?>" width="160"></td>
-                    <td colspan="2">Nombre: <?php echo $propietario ?></td>
+                    <td align="center">Nombre: <?php echo $propietario ?></td>
                 </tr>
                 <tr>
-                    <td>Fecha y Hora</td>
-                    <td><?php echo date("d-m-Y H:i A", strtotime("" . $fechafinal)); ?></td>
+                    <td align="center"><b>Fecha y hora de entrega de su trámite:</b></td>
+                </tr>
+                 <tr>
+                    <td align="center"><b><?php  echo  $fechaEntrega;/*date("d-F-Y H:i A", strtotime("" . $fechafinal));*/ ?></b></td>
                 </tr>
                 <tr>
-                    <td>Solicitud:</td>
-                    <td>Número de Folio:</td>
+                    <td align="center">Número de Folio:<?php echo $orden ?></td>
+                   </tr>
+                <tr>
+                    <td align="center"><img src="<?php echo base_url('assets/images/logo.png   ') ?>" width="160">
+                    </td>
                 </tr>
                 <tr>
-                    <td><label > <?php if ($tipotramite == 1): ?>
-                                <?php echo "ASIGNACIÓN DE CLAVE CATASTRAL"; ?>
-                            <?php elseif ($tipotramite == 2): ?>
-                                <?php echo "MODIFICACIÓN DE CLAVE CATASTRAL"; ?>
-                            <?php elseif ($tipotramite == 3): ?>
-                                <?php echo "DUPLICADO DE CLAVE CATASTRAL"; ?>
-                            <?php endif; ?>
+                    <td align="center">
+                        <label class="pie">
+                            Calle Álvaro Obregón N°100 Zona Centro, Irapuato Guanajuato,
                         </label>
                     </td>
-                    <td><?php echo $orden ?></td>
                 </tr>
                 <tr>
-                    <td colspan="2">
-                        <label class="pie"> 
-                            Calle Álvaro Obregón N°100 Zona Centro, Irapuato Guanajuato, Tel: 01(462)6069999 ext. 1564
-                        </label>
-                    </td>
-
+                    <td align="center">Tel: 01(462)6069999 ext. 1564</td>
                 </tr>
             </table>
         </div>
