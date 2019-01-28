e591c9549a8c611868f7e506d584b4ae07713548
Fernando <f.vargas.bravo@icloud.com>
2019-01-28 15:02:18 -0600

    Se quitò el primer boton de orden de pago

--- VUIRA1.5/c_clave_fraccionamientos/Clave_Catastral_Fraccionamientos_Auxiliar.php
+++ VUIRA1.5/c_clave_fraccionamientos/Clave_Catastral_Fraccionamientos_Auxiliar.php
@@ -363,11 +363,6 @@ ini_set("display_errors",1);
 
 										              </div><!-- FIN SUPERFICE-->
 										            </div>
-													<!-- <div class="row">
-														<div class="col-md-12" align="right">
-															<input type="button" class="btn btn-success" value="">
-														</div>
-													</div> -->
 													<div class="row">
 														<div class="col-md-12" align="right">
 															<input type="button" class="btn btn-success" value="Documento del pago total de cuentas" onclick="event_mostrar_modal_pago_cuentas()">
